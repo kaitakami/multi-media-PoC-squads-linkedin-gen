7ac109a052af20e767304ebb283b7ce0b2e0a8b0
Kai Takami <83680466+kaitakami@users.noreply.github.com>
2024-07-08 13:20:16 +0000

    feat: change to gpt-4o

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 # Create a Deepgram client
 deepgram = DeepgramClient(os.getenv("DEEPGRAM_API_KEY"))
 
-OPENAI_MODEL = "gpt-3.5-turbo"
+OPENAI_MODEL = "gpt-4o"
 
 st.title("📝 SquadS Ventures PoC Generador de Publicaciones de LinkedIn/Blog")
 st.write(
